4872ed24d10c511fae07c2d1c192cfcdc9efacc0
Gaëlle BROSSAS <gaellebrossas@orange.fr>
2020-06-11 10:53:54 +0200

    TP : encapsulation

---
+++ src/main/java/fr/diginamic/banque/entites/Compte.java
@@ -0,0 +1,25 @@
+package fr.diginamic.banque.entites;
+
+public class Compte {
+	
+	private int numeroCompte;
+	private double solde;
+	
+	public int getNumeroCompte() {
+		return numeroCompte;
+	}
+
+	public double getSolde() {
+		return solde;
+	}
+
+	private void setSolde(double nvSolde) {
+		solde = nvSolde;
+	}
+
+	public Compte(int compte, double solde) {
+		this.numeroCompte = compte;
+		this.solde = solde;
+	}
+	
+}
